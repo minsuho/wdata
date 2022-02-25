e17aeae48bb15ae35d641dde93339e56e34a385a
LeeMinHo <58880552+minsuho@users.noreply.github.com>
2022-02-25 22:21:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def hello_world11():
     json_data = json.load(json_file)
   return jsonify({'테스트':'테스트','테스트':'테스트','테스트':'테스트','테스트':'테스트'})
 
-app.run(debug=True, host='0.0.0.0', port=8000)
+app.run(debug=True)
